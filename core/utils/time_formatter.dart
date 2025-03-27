6cfb82c66006fe79b51152291e9ca0fad1487030
aliazimoshan <a.azimoshan@gmail.com>
2025-03-28 02:27:02 +0330

    remove wrong copied files

--- core/utils/time_formatter.dart
+++
@@ -1,6 +0,0 @@
-class TimeFormatter {
-  static String formatTime(String rawTime) {
-    // move the time formatting from here to ios native module
-    return rawTime;
-  }
-}
--- modules/home/domain/home_repository_interface.dart
+++
@@ -1,6 +0,0 @@
-import 'package:zanis_task/modules/home/data/dto/home_dto.dart';
-
-abstract class HomeRepositoryInterface {
-  Future<HomeDTO> fetchCurrentTime();
-  Stream<String> fetchUSBDataStream();
-}
